5188429a29d9ee8aef0bd566897f5394d45b1837
David LJ <4dlj1995@gmail.com>
2020-11-08 17:40:55 -0600

    Basic select text and save to file functionality
    On click on extension's button, get the text selected by user (1), and
    set it as output div inner contents (2), which will be shown as a
    popup under extension's icon (3).
    
    On popup.js script execution, dynamically set download url to save the
    selected text's content to disk.
    
    1. script/get_selection.js
    2. popup.js
    3. popup.html

--- popup.js
+++ popup.js
@@ -1,6 +1,29 @@
-  chrome.tabs.executeScript({
-      code: "window.getSelection().toString();"
-    }, function(selection) {
-      console.log(selection);
-      document.getElementById("output").innerHTML = selection[0];
-  });
+/* where text will be shown */
+output = document.getElementById("output");
+
+downloadBtn = document.getElementById('download');
+
+function returnUri(myString) {
+	let fileContents = encodeURIComponent(myString);
+	let = uriContents = 'data:text/csv;charset=utf-8,' + fileContents;
+	return uriContents;
+}
+
+/* selected text as #output's contents */
+function setPopup(selection) {
+	output.innerHTML = selection[0];
+
+	/* on click, save text as file */
+	let dateStr = new Date().toISOString().slice(0,19).replace(/-/g, "");
+
+	downloadBtn.setAttribute("download", dateStr + '.txt');
+	downloadBtn.setAttribute("href", returnUri(output.innerHTML));
+}
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	chrome.tabs.executeScript(tabs[0].id, {
+		"file" : "scripts/get_selection.js"
+	}, function(selection) {
+		setPopup(selection);
+	});
+});
