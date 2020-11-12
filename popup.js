02815e5fc6f9830306ea254872e7c50c151503a5
David LJ <4dlj1995@gmail.com>
2020-11-11 20:55:28 -0600

    Improvements in the way clipped content is saved
    popup.js

--- popup.js
+++ popup.js
@@ -1,39 +1,64 @@
 /* where text will be shown */
 display = document.getElementById("display");
 
-richText = document.getElementById("rich-text");
-
+/* download button */
 downloadBtn = document.getElementById('download');
 
-fileType = document.getElementById('file-type-select');
+/* select file type */
+fileTypeSelector = document.getElementById('file-type-select');
 
 /* on change file type, prepare change url accordingly */
-fileType.addEventListener('change', function(){
-	setFileSave(this.value);
+fileTypeSelector.addEventListener('change', function(){
+	setUpClipboard(setFileSave);
 });
 
-function setFileSave(fileType = '') {
+function setFileSave(data = false) 
+{
+	/* there is no data  */
+	if (!data) { 
+		let mustHide = document.getElementsByClassName('on-selection');
+
+		for (let i = 0; i < mustHide.length; i++) {
+			mustHide[i].style.display = 'none';
+		}
+
+		let mustShow = document.getElementsByClassName('no-selection');
+
+		for (let i = 0; i < mustShow.length; i++) {
+			mustShow[i].style.display = '';
+		}
+
+		return;
+	}
+
 	file = (function() {
 		let name = new Date().toISOString().slice(0,19).replace(/-/g, "");
 		let mediaType = '';
 		let encoding = 'charset=utf-8,';
 
-		/* Convert the DOM to plaintext or markdown */
-		let contents = encodeURIComponent(display.innerHTML);
+		let contents = {
+			'md' : JSON.parse(data.dom),
+			'html' : JSON.parse(data.dom),
+			'txt' : data.string
+		}
+
+		let fileType = fileTypeSelector.value ?? '';
 
 		switch (fileType) {
-			case '1':
-				mediaType += 'text/plain;';
-				name += '.txt';
-				break;
 			case '2':
 				mediaType += 'text/markdown;';
 				name += '.md';
+				uri = 'data:' + mediaType + encoding + contents.md
 				break;
 			case '3':
 				mediaType += 'text/html;';
 				name += '.html';
+				uri = 'data:' + mediaType + encoding + contents.html
 				break;
+			default:
+				mediaType += 'text/plain;';
+				name += '.txt';
+				uri = 'data:' + mediaType + encoding + contents.txt
 		}
 
 	    return {
@@ -41,7 +66,7 @@ function setFileSave(fileType = '') {
 	    	'mediaType' : mediaType,
 	    	'contents' : contents, 
 	    	'encoding' : encoding, 
-	    	'uri' : 'data:' + mediaType + encoding + contents,
+	    	'uri' : uri,
 	    }
 	})();
 
@@ -53,13 +78,13 @@ function setFileSave(fileType = '') {
 function setPopup(selection) 
 {
 	if (!selection.string || !selection.dom) {
-		mustHide = document.getElementsByClassName('on-selection');
+		let mustHide = document.getElementsByClassName('on-selection');
 
 		for (let i = 0; i < mustHide.length; i++) {
 			mustHide[i].style.display = 'none';
 		}
 
-		mustShow = document.getElementsByClassName('no-selection');
+		let mustShow = document.getElementsByClassName('no-selection');
 
 		for (let i = 0; i < mustShow.length; i++) {
 			mustShow[i].style.display = '';
@@ -69,14 +94,15 @@ function setPopup(selection)
 	}
 
 	display.innerHTML = selection.string;
-	richText.innerHTML = JSON.parse(selection.dom);
 }
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	chrome.tabs.executeScript(tabs[0].id, {
+function setUpClipboard (callback) {
+	chrome.storage.local.get(['string', 'dom'], callback);
+}
+
+chrome.tabs.executeScript({
 		"file" : "content_scripts/get_selection.js"
 	}, function(result) {
 		setPopup(result[0]);
-		setFileSave();
-	});
+		setUpClipboard(setFileSave);
 });
